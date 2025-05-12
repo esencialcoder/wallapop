d118e399436520031a12eebb2b5dc55b711bce73
Juan Manuel <juanmanuel31@hotmail.com>
2025-05-12 17:03:16 +0200

    create links in buttons login, registration, etc.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import { adListController } from "./ad-list/adListController.js";
 
 const adListElement = document.querySelector("#ads-list");
+const loginBtn = document.querySelector('#loginBtn');
+const signupBtn = document.querySelector('#signupBtn');
 
+adListController(adListElement);
 
+if (signupBtn) {
+    signupBtn.addEventListener('click', () => {
+        window.location.href = 'signup.html'
+    })
+    
+}
+if (loginBtn) {
+    loginBtn.addEventListener('click', () => {
+        window.location.href = 'signup.html'
+    })
+    
+}
 
-adListController(adListElement)
