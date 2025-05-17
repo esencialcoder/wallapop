3b48dd2392c699edba2b9d9cd803a69479aa4d3d
Juan Manuel <juanmanuel31@hotmail.com>
2025-05-18 00:31:15 +0200

    login refactor

--- login/index.js
+++ login/index.js
@@ -1,14 +1,18 @@
+import { pubSub } from '../pubSub.js';
 import { initLoginController } from './loginController.js';
 
 const form = document.querySelector('#login-form');
-
 const messageContainer = document.querySelector('#login-messages');
 
+// Inicializa sistema global de notificaciones aquí (si no lo has hecho ya)
+import { initNotificationsController } from '../notifications/notificationsController.js';
+initNotificationsController(messageContainer, pubSub);
+
+// Mostrar mensaje de registro exitoso si existe
 const successMessage = localStorage.getItem('signupSuccess');
 if (successMessage) {
-  import('../signup/signupView.js').then(({ showSuccessMessage }) => {
-    showSuccessMessage(messageContainer, successMessage);
-    localStorage.removeItem('signupSuccess');
-  });
+  pubSub.publish('SUCCESS', successMessage);
+  localStorage.removeItem('signupSuccess');
 }
-initLoginController(form);
+
+initLoginController(form, pubSub);
