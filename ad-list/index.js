23640a2b18cc3d2e86a5682bba065c71cca97af2
Juan Manuel <juanmanuel31@hotmail.com>
2025-05-17 22:24:00 +0200

    ad-list refactor

--- ad-list/index.js
+++ ad-list/index.js
@@ -1,4 +1,5 @@
 import { initAdController } from './adListController.js';
+import { pubSub } from '../pubSub.js';
 
 const adsListElement = document.querySelector('#ads-list');
-initAdController(adsListElement);
+initAdController(adsListElement, pubSub);
