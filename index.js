1b56aa05f87730ac83695d9e463fcd87bf4bc35e
Juan Manuel <juanmanuel31@hotmail.com>
2025-05-20 00:08:07 +0200

    changes

--- index.js
+++ index.js
@@ -5,22 +5,56 @@ import { initNotificationsController } from './notifications/notificationsContro
 const adListElement = document.querySelector("#ads-list");
 const loginBtn = document.querySelector('#loginBtn');
 const signupBtn = document.querySelector('#signupBtn');
+const logoutBtn = document.querySelector('#logoutBtn'); 
 const notificationsElement = document.querySelector('#notifications');
 
 initAdsController(adListElement, pubSub);
 initNotificationsController(notificationsElement, pubSub);
 
-
 if (signupBtn) {
     signupBtn.addEventListener('click', () => {
-        window.location.href = 'signup.html'
-    })
-    
-}
-if (loginBtn) {
-    loginBtn.addEventListener('click', () => {
-        window.location.href = 'login.html'
-    })
-    
+        window.location.href = 'signup.html';
+    });
 }
 
+if (loginBtn) {
+    loginBtn.addEventListener('click', () => {
+        window.location.href = 'login.html';
+    });
+}
+
+const createAdBtn = document.getElementById("createAdBtn");
+
+const token = localStorage.getItem('token');
+
+if (token) {
+    createAdBtn.classList.remove("d-none");
+    if (logoutBtn) logoutBtn.classList.remove("d-none");
+    if (loginBtn) loginBtn.classList.add("d-none");
+    if (signupBtn) signupBtn.classList.add("d-none");
+} else {
+    createAdBtn.classList.add("d-none");
+    if (logoutBtn) logoutBtn.classList.add("d-none");
+    if (loginBtn) loginBtn.classList.remove("d-none");
+    if (signupBtn) signupBtn.classList.remove("d-none");
+}
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+        localStorage.removeItem("token");
+        pubSub.publish('SUCCESS', 'Has cerrado sesión correctamente.');
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 1000); 
+    });
+}
+
+createAdBtn.addEventListener("click", () => {
+    const token = localStorage.getItem('token'); 
+    if (token) {
+        window.location.href = "./create-ads.html";
+    } else {
+        pubSub.publish('ERROR', 'Debes estar logueado para crear un anuncio.');
+        window.location.href = "./login.html";
+    }
+});
